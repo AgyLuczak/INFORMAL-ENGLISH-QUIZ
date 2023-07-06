1c4729b261010b204bb8a4c69a9ce5c0a0be0c7d
AgyLuczak <aaluczak1@gmail.com>
2023-07-06 12:53:06 +0000

    Remove the loop and create a global variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,18 @@ const option1 = document.getElementById('a')
 const option2 = document.getElementById('b')
 const option3 = document.getElementById('c')
 const option4 = document.getElementById('d')
+const nextBtn = document.getElementById('next-btn')
 
+// to keep track of question number
+var q = 0
 
-function displayQuestions() {
+function displayQuestion() {
     console.log(questions.length)
     // add the value of each question to the questionText tag
-    for (let i = 0; i < questions.length; i++) {
-        questionText.innerHTML = questions[i].question
-        option1.innerHTML = questions[i].choice1
-        option2.innerHTML = questions[i].choice2
-        option3.innerHTML = questions[i].choice3
-        option4.innerHTML = questions[i].choice4
-    }
+
+    questionText.innerHTML = questions[q].question
+    option1.innerHTML = questions[q].choice1
+    option2.innerHTML = questions[q].choice2
+    option3.innerHTML = questions[q].choice3
+    option4.innerHTML = questions[q].choice4
 }
-displayQuestions()
