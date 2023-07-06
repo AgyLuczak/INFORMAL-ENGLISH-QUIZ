5e2fa8c764b549a68c42db4bcf0bd4020baa3a43
AgyLuczak <aaluczak1@gmail.com>
2023-07-06 10:28:23 +0000

    Add function to display questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
-const question = document.getElementById("question");
-const answers = Array.from(document.getElementsByClassName("answer-text"));
-console.log(answers);
+// const question = document.getElementById("question");
+// const answers = Array.from(document.getElementsByClassName("answer-text"));
+// console.log(answers);
+// let currentQuestion = {};
+// let correctAnswers = true;
+// let availableQuestions = [];
+// const scorePoints = 100;
+// const maxQuestions = 10;
 
-let currentQuestion = {};
-let correctAnswers = true;
-let availableQuestions = [];
-const scorePoints = 100;
-const maxQuestions = 10;
+
+
+console.log("Questions", questions)
+
+function displayQuestions() {
+
+    for (let i = 0; i < questions.length; i++) {
+        console.log(questions[i])
+    }
+}
+
+displayQuestions()
