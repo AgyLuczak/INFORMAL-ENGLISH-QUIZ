8d94e9101e313a96828417738df619a65e7b7d2b
AgyLuczak <aaluczak1@gmail.com>
2023-07-09 11:40:15 +0000

    Correct a typo in the displayQuestion function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,31 @@
+/* const questionText = document.getElementById("question-text");
+const option1 = document.getElementById('a')
+const option2 = document.getElementById('b')
+const option3 = document.getElementById('c')
+const option4 = document.getElementById('d')
+const nextBtn = document.getElementById('next-btn')
+// to keep track of question number
+var q = 0
+
+function displayQuestion() {
+    console.log(questions.length)
+    console.log("q=", q)
+    // add the value of each question to the questionText tag
+
+    questionText.textContent = questions[q].question
+    option1.textContent = questions[q].choice1
+    option2.textContent = questions[q].choice2
+    option3.textContent = questions[q].choice3
+    option4.textContent = questions[q].choice4
+}
+
+nextBtn.addEventListener('click', function () {
+    q++;
+    displayQuestion();
+})
+
+// displayQuestion()*/
+
 const questionText = document.getElementById("question-text");
 const option1 = document.getElementById('a')
 const option2 = document.getElementById('b')
@@ -24,4 +52,4 @@ nextBtn.addEventListener('click', function () {
     displayQuestion();
 })
 
-displayQuestions()
+displayQuestion()
