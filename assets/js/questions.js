e939e7841c9ef81e68a5dde5723d35d2b032ed8d
AgyLuczak <aaluczak1@gmail.com>
2023-07-19 12:39:48 +0000

    Create a separate score.js page

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -13,69 +13,69 @@ let questions = [{
         choice3: "He fooled us",
         choice4: "He got us interested in sewing",
         answer: 'b'
-    },
-    {
-        question: "Ta!",
-        choice1: "Thank you!",
-        choice2: "Bye",
-        choice3: "Yes!",
-        choice4: "Right, I see.",
-        answer: 'a'
-    },
-    {
-        question: "These people are minted.",
-        choice1: "These people are poor.",
-        choice2: "These people smell very nice.",
-        choice3: "These people are crazy.",
-        choice4: "These people are very rich.",
-        answer: 'd'
-    },
-    {
-        question: "What do you reckon?",
-        choice1: "Do you wish to return something?",
-        choice2: "What do you think?",
-        choice3: "Do you suffer from reflux?",
-        choice4: "What is your favourite record?",
-        answer: 'b'
-    },
-    {
-        question: "I'm knackered.",
-        choice1: "I'm very tired.",
-        choice2: "I'm all bruised.",
-        choice3: "I'm behind with work.",
-        choice4: "I'm very sad.",
-        answer: 'a'
-    },
-    {
-        question: "Are yoy taking the mickey?",
-        choice1: "Are you taking it seriously?",
-        choice2: "Do you like Mickey Mouse?",
-        choice3: "Are you joking?",
-        choice4: "Are you taking your mouse to the vet?",
-        answer: 'c'
-    },
-    {
-        question: "My wife is doing my head in.",
-        choice1: "My wife makes me a better man.",
-        choice2: "My wife hits me on my head a lot.",
-        choice3: "My wife is annoying me.",
-        choice4: "My wife tells me to be quiet.",
-        answer: 'c'
-    },
-    {
-        question: "What time do you make it?",
-        choice1: "What time will you get there?",
-        choice2: "What's the time?",
-        choice3: "What time do you finish work?",
-        choice4: "Why are you late?",
-        answer: 'b'
-    },
-    {
-        question: "What are you up to?",
-        choice1: "What are you looking at?",
-        choice2: "Are you in trouble?",
-        choice3: "How much money do you owe?",
-        choice4: "What are you doing right now?",
-        answer: 'd'
     }
+    // {
+    //     question: "Ta!",
+    //     choice1: "Thank you!",
+    //     choice2: "Bye",
+    //     choice3: "Yes!",
+    //     choice4: "Right, I see.",
+    //     answer: 'a'
+    // },
+    // {
+    //     question: "These people are minted.",
+    //     choice1: "These people are poor.",
+    //     choice2: "These people smell very nice.",
+    //     choice3: "These people are crazy.",
+    //     choice4: "These people are very rich.",
+    //     answer: 'd'
+    // },
+    // {
+    //     question: "What do you reckon?",
+    //     choice1: "Do you wish to return something?",
+    //     choice2: "What do you think?",
+    //     choice3: "Do you suffer from reflux?",
+    //     choice4: "What is your favourite record?",
+    //     answer: 'b'
+    // },
+    // {
+    //     question: "I'm knackered.",
+    //     choice1: "I'm very tired.",
+    //     choice2: "I'm all bruised.",
+    //     choice3: "I'm behind with work.",
+    //     choice4: "I'm very sad.",
+    //     answer: 'a'
+    // },
+    // {
+    //     question: "Are yoy taking the mickey?",
+    //     choice1: "Are you taking it seriously?",
+    //     choice2: "Do you like Mickey Mouse?",
+    //     choice3: "Are you joking?",
+    //     choice4: "Are you taking your mouse to the vet?",
+    //     answer: 'c'
+    // },
+    // {
+    //     question: "My wife is doing my head in.",
+    //     choice1: "My wife makes me a better man.",
+    //     choice2: "My wife hits me on my head a lot.",
+    //     choice3: "My wife is annoying me.",
+    //     choice4: "My wife tells me to be quiet.",
+    //     answer: 'c'
+    // },
+    // {
+    //     question: "What time do you make it?",
+    //     choice1: "What time will you get there?",
+    //     choice2: "What's the time?",
+    //     choice3: "What time do you finish work?",
+    //     choice4: "Why are you late?",
+    //     answer: 'b'
+    // },
+    // {
+    //     question: "What are you up to?",
+    //     choice1: "What are you looking at?",
+    //     choice2: "Are you in trouble?",
+    //     choice3: "How much money do you owe?",
+    //     choice4: "What are you doing right now?",
+    //     answer: 'd'
+    // }
 ];
